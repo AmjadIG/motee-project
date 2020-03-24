6990ed42b708f5bee67ed52963c5739822c0bbc2
AmjadIG <amjadmenouer@gmail.com>
2020-03-24 19:56:05 +0100

    proposition filtered

--- ios/Motee/Motee/Models/AnswerModel.swift
+++ ios/Motee/Motee/Models/AnswerModel.swift
@@ -14,6 +14,7 @@ class AnswerModel {
     
     //Get All : https://mootee-api.herokuapp.com/answers
     //Get by id : https://mootee-api.herokuapp.com/answers/id
+    //Post : https://mootee-api.herokuapp.com/answers/newAnswer => Pas encore testés... fail to authenticate token... need to get token from user
     
     static func getAll()->[String:Answer]{
         // Prepare URL
@@ -95,6 +96,56 @@ class AnswerModel {
     
     static func getPropOfAnswer(answer : Answer)->Proposition{
         return (purifyRequest(dictionary: PropositionModel.getPropositionById(idProp: answer.idProposition))[0] as! Proposition)
+    }
+    
+    
+    //???
+    static func addAnswer (ans : Answer) -> Bool{
+        // Prepare URL
+        //guard let token = currentUser?.authToken else{return false}
+        
+        let stringurl = "https://mootee-api.herokuapp.com/answers/newAnswer"
+        let url = URL(string: stringurl)//ICI
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        let semaphore = DispatchSemaphore(value :0)
+        var res : Bool = false
+        // Set HTTP Request Body
+        
+        do{
+            //try  print(JSONSerialization.jsonObject(with: JSONEncoder().encode(rem), options: []))
+            //let json = try JSONSerialization.jsonObject(with: JSONEncoder().encode(rem), options: [])
+            request.httpBody = try JSONEncoder().encode(ans)
+            
+        }catch let error {
+            print(error)
+        }
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        print("json : " , String(data : request.httpBody!, encoding: .utf8)!)
+        // Perform HTTP Request
+         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+                
+                let resp = response as? HTTPURLResponse
+            print("code d'erreur")
+                res = (resp?.statusCode == 200)
+                
+            if let data = data{
+                if let jsonString = String(data: data, encoding: .utf8){
+                    print(jsonString)
+                }
+            }
+            semaphore.signal()
+        }
+        task.resume()
+        semaphore.wait()
+        return res
     }
 
 }
--- ios/Motee/Motee/Models/UserModel.swift
+++ ios/Motee/Motee/Models/UserModel.swift
@@ -10,6 +10,11 @@ import Foundation
 
 class UserModel {
     
+    //Model for User :
+    
+    //Get All : https://mootee-api.herokuapp.com/users
+    //Get by id : https://mootee-api.herokuapp.com/users/id
+    
     static func getAll()->[String:User]{
         // Prepare URL
         let stringURL = "https://mootee-api.herokuapp.com/users"
@@ -49,8 +54,42 @@ class UserModel {
         return res
     }
     
-    static func getUserById(idUser : String)->User?{
-        return getAll()[idUser]
+    static func getUserById(idUser : String)->User{
+        // Prepare URL
+        let stringURL = "https://mootee-api.herokuapp.com/users/"+idUser
+        let url = URL(string: stringURL)
+        print("in getAll")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object (GET)
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "GET"
+        let semaphore = DispatchSemaphore(value :0)
+        print("request ok")
+        // Perform HTTP Request
+        var res : [String:User] = [:]
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+            // Check for Error
+            if let error = error {
+                print("Error took place :\(error)")
+                return
+            }
+        
+            // Convert HTTP Response Data to a String
+            if let data = data{
+                do{
+                    res = try JSONDecoder().decode([String:User].self, from: data)
+                    print("decoder ok!")
+                }catch let error {
+                    print(error)
+                }
+            }
+            semaphore.signal()
+        }
+        task.resume()
+        
+        semaphore.wait()
+        return purifyRequest(dictionary: res)[0] as! User
     }
     
     static func getPropositionsByUser(user : User)->[Proposition]{
